0df4bbe1a3d7f1424ad1c671ec2e9420e95caa2a
Alex Eremin <ifollowalex@gmail.com>
2024-03-18 12:06:27 +0600

    second File

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,6 +6,7 @@ export default function Header(){
    return (
        <div className="container">
            <h1 style={{textAlign: 'center', marginTop: '30px'}}>Добро пожаловать на тест по массивам JS</h1>
+           <p>как дела</p>
        </div>
    ) 
 }
